16a32c6a60c739fb2aada05cdc4d6a3a9ba3c3db
outtaspace <flyweight@yandex.ru>
2017-01-09 19:32:15 +0200

    Вынес из app всю специфику миграций, теперь app не знает про миграции, но скрипт миграции использует app.

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import os
 
 from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+
 from flask_sqlalchemy import SQLAlchemy
 
 
 def configure(app):
-    config = {
-        'development': 'config.DevelopmentConfig',
-        'testing': 'config.TestingConfig',
-        'production': 'config.ProductionConfig',
-        'default': 'config.DevelopmentConfig'
-    }
+    config = dict(
+        development='config.DevelopmentConfig',
+        testing='config.TestingConfig',
+        production='config.ProductionConfig',
+        default='config.DevelopmentConfig'
+    )
     domain = os.getenv('FLASK_CONFIGURATION', 'default')
     app.config.from_object(config[domain])
 
@@ -21,11 +20,6 @@ app = Flask(__name__)
 configure(app)
 
 db = SQLAlchemy(app)
-
-migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 from articles.blueprint import articles # noqa
 app.register_blueprint(articles)
--- manage.py
+++ manage.py
@@ -1,4 +1,10 @@
-from app import manager
+from app import app, db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 if __name__ == '__main__':
+    migrate = Migrate(app, db)
+
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
     manager.run()
