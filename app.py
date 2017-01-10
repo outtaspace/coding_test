33029e119e673e05c411275c8537174faff05344
outtaspace <flyweight@yandex.ru>
2017-01-10 18:03:29 +0200

    Выделил метод register_extensions.

--- app.py
+++ app.py
@@ -16,14 +16,18 @@ def configure(app):
     app.config.from_object(config[domain])
 
 
+def register_extensions(app):
+    db.init_app(app)
+
+
 def register_blueprints(app):
     from articles.blueprint import articles
     app.register_blueprint(articles)
 
 
+db = SQLAlchemy()
+
 app = Flask(__name__)
 configure(app)
-
-db = SQLAlchemy(app)
-
+register_extensions(app)
 register_blueprints(app)
